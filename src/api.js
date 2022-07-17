ef666a3f205cacd0c0df840a50b0b3aaa6dc9c75
Jamie Finlay <jpfinlay@gmail.com>
2022-07-17 20:41:39 +0100

    Initial commit with working city lookup functionality

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '764fb4bbd4msh81dd1dbc72edc99p15cc42jsnf2d656166066',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
